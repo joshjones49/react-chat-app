99ce8c80dc5c9c828dd58ccb58f656297d27c6b7
joshjones49 <joshjones200285@gmail.com>
2025-08-05 20:55:26 -0400

    UI for Chat page completed

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,28 @@
 import './RightSidebar.css'
 
+import assets from '../../assets/assets'
+
 const RightSidebar = () => {
   return (
-    <div>
-      
+    <div className='rs' >
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Rich Sanford <img src={assets.green_dot} className='dot' alt="" /> </h3>
+        <p>Hey, I am Richard Sanford using the chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
     </div>
   )
 }

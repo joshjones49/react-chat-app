e29b0fab9c948733c4acdb0160e0cc155d8f4906
joshjones49 <joshjones200285@gmail.com>
2025-08-06 14:44:34 -0400

    Edit profile functional

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -2,24 +2,93 @@ import './ProfileUpdate.css'
 
 import assets
 from '../../assets/assets'
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth, db } from '../../config/firebase.mjs'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import upload from '../../lib/upload.mjs'
+import { AppContext } from '../../context/AppContext'
 
 const ProfileUpdate = () => {
 
     const [image, setImage] = useState(false)
+    const [name, setName] = useState('')
+    const [bio, setBio] = useState('')
+    const [uid, setUid] = useState('')
+    const [prevImage, setPrevImage] = useState('')
+
+    const { setUserData } = useContext(AppContext)
+
+    const profileUpdate = async (event) => {
+      event.preventDefault()
+      try {
+        if(!prevImage && !image) {
+          toast.error("Upload Profile Picture")
+        }
+        const docRef = doc(db, 'users', uid)
+        if(image) {
+          const imgURL = await upload(image)
+          setPrevImage(imgURL)
+          await updateDoc(docRef, {
+            avatar: imgURL,
+            bio: bio,
+            name: name
+          })
+        } else {
+          await updateDoc(docRef, {
+            bio: bio,
+            name: name
+          })
+        }
+        const snap = await getDoc(docRef)
+        setUserData(snap.data())
+        navigate('/chat')
+      } catch (error) {
+        console.error(error)
+        toast.error(error.message)
+      }
+    }
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+      onAuthStateChanged(auth, async (user) => {
+        if(user) {
+          setUid(user.uid)
+          const docRef = doc(db, 'users', user.uid)
+          const docSnap = await getDoc(docRef)
+
+          if(docSnap.data().name) {
+            setName(docSnap.data().name)
+          }
+
+          if(docSnap.data().bio) {
+            setBio(docSnap.data().bio)
+          }
+
+          if (docSnap.data().avatar) {
+            setPrevImage(docSnap.data().avatar)
+          }
+        } else {
+          navigate('/')
+        }
+      })
+    }, [])
 
   return (
     <div className='profile pages' >
       <div className="profile-ctn">
-        <form>
+        <form onSubmit={profileUpdate} >
             <h3>Profile Details</h3>
             <label htmlFor="avatar">
                 <input onChange={(e) => setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
                 <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
                 upload profile image
             </label>
-            <input type="text" placeholder='Name' required/>
-            <textarea placeholder='Bio' required></textarea>
+            <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder='Name' required/>
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder='Bio' required></textarea>
             <button type='submit' >Save</button>
         </form>
         <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
